4aca42ef86c60e2bc6788a8effa087cc32f79d45
betu55 <bemenet.bekele@torontomu.ca>
2024-09-20 10:59:29 -0400

    CPS721 assignment one base files

---
+++ q1_bank_kb.pl
@@ -0,0 +1,27 @@
+
+% Enter the names of your group members below.
+% If you only have 2 group members, leave the last space blank
+%
+%%%%%
+%%%%% NAME: 
+%%%%% NAME:
+%%%%% NAME:
+%
+% Add the required rules in the corresponding sections. 
+% If you put the rules in the wrong sections, you will lose marks.
+%
+% You may add additional comments as you choose but DO NOT MODIFY the already included comment lines below
+%
+
+month(1). month(2). month(3). month(4). month(5). month(6).
+month(7). month(8). month(9). month(10). month(11). month(12).
+
+%%%%% SECTION: q1_kb
+%%%%% You should put the atomic statements in your KB below in this section
+
+
+
+
+
+%%%%% END
+% DO NOT PUT ANY ATOMIC PROPOSITIONS OR LINES BELOW
