c530d59b4dd39917fc828e7419f1ffbb5a952c34
soupe1 <77170782+soupe1@users.noreply.github.com>
2024-09-24 11:02:02 -0400

    Update q1_bank_kb.pl
    added kb for q1

--- q1_bank_kb.pl
+++ q1_bank_kb.pl
@@ -3,7 +3,7 @@
 % If you only have 2 group members, leave the last space blank
 %
 %%%%%
-%%%%% NAME: 
+%%%%% NAME: Subha Tasnim
 %%%%% NAME:
 %%%%% NAME:
 %
@@ -20,7 +20,48 @@ month(7). month(8). month(9). month(10). month(11). month(12).
 %%%%% You should put the atomic statements in your KB below in this section
 
 
+hasAccount(ada_lovelace, cibc, 4000).
+hasAccount(alan_turing, cibc, 3000).
+hasAccount(boba_fett, scotiabank, 200).
+hasAccount(boba_fett, bmo, 300).
+hasAccount(naruto_uzumaki, wells_fargo, 3000).
+hasAccount(sasuke_uchiha, cibc, 6000).
+hasAccount(harry_potter, scotiabank, 200).
+hasAccount(rick_morty, scotibank, 10).
+hasAccount(anakin_skywalker, bmo, 200).
+hasAccount(anakin_skywalker, td, 3000).
+hasAccount(anakin_skywalker, national_bank_of_canada, 90).
+hasAccount(judy_hopps, national_bank_of_canada, 60000).
+hasAccount(boba_fett, td, 400).
+hasAccount(jango_fett, scotiabank, 222).
 
+
+lives(ada_lovelace, san_francisco).
+lives(alan_turing, toronto).
+lives(boba_fett, kamino).
+lives(naruto_uzumaki, konoha).
+lives(sasuke_uchiha, new_york).
+lives(harry_potter, toronto).
+lives(rick_morty, winnipeg).
+lives(anakin_skywalker, naboo).
+lives(judy_hopps, zootopia).
+lives(jango_fett, kamino).
+
+
+created(ada_lovelace, cibc, 11, 2013).
+created(alan_turing, cibc, 4, 2019).
+created(boba_fett, scotiabank, 1, 2024).
+created(boba_fett, bmo, 1, 2023).
+created(naruto_uzumaki, wells_fargo, 3, 2015).
+created(sasuke_uchiha, cibc, 4, 2018).
+created(harry_potter, cibc, 1, 2024).
+created(rick_morty, scotiabank, 1, 2024).
+created(anakin_skywalker, bmo, 3, 2001).
+created(anakin_skywalker, td, 4, 2024).
+created(anakin_skywalker, national_bank_of_canada, 9, 2024).
+created(judy_hopps, national_bank_of_canada, 2, 2017).
+created(boba_fett, wells_fargo, 4, 2022).
+created(jango_fett, scotiabank, 8, 2002).
 
 
 %%%%% END
